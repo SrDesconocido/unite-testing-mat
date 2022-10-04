f7b57216a703738c12bacb9299b9a5fe0740b2c0
SrDesconocido <octaviopinm@gmail.com>
2022-10-04 09:38:39 -0500

    update compute #1

--- src/app/compute/compute.spec.ts
+++ src/app/compute/compute.spec.ts
@@ -1,4 +1,4 @@
-import {compute} from './compute';
+import { compute } from './compute';
 
 describe('compute test suite', () =>{
     it('Should return 0 if input is negative', () => {
@@ -7,7 +7,7 @@ describe('compute test suite', () =>{
     })
 
     it('Should increment the imput if it is positive ',()=> {
-        const result =compute(-1);
+        const result =compute(1);
         expect(result).toBe(2);
     })
 })
