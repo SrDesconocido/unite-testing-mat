4b52a9256547bd30d4b3c00224e39e77a9d181a5
SrDesconocido <octaviopinm@gmail.com>
2022-10-07 11:11:39 -0500

    check pow #7

--- src/app/pow/pow.spec.ts
+++ src/app/pow/pow.spec.ts
@@ -13,11 +13,11 @@ describe('pow', () => {
 
 	it ('Should return 8 if x=2, n=3', () => {
 		const result = pow(2, 3);
-		expect(result).toBe(0);
+		expect(result).toBe(8);
 	})
 	
 	it ('Should return 9 if x=3, n=2', () => {
 		const result = pow(3, 2);
-		expect(result).toBe(0);
+		expect(result).toBe(9);
 	})
 });
